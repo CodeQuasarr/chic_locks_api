a6aa39c72b65ce6c6cbbbfbd5a8277e3d5d75c4b
Samuel ANYARONKE <75504658+CodeQuasarr@users.noreply.github.com>
2025-03-04 09:09:59 +0100

    test(users): ajout d'un test pour la modification des informations des utilisateurs

---
+++ tests/Feature/User/EditingUserInfoTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+use Laravel\Sanctum\Sanctum;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+describe('Editing user information', function () {
+
+    beforeEach(function () {
+        // init roles and permissions
+        Artisan::call('update:roles-permissions');
+
+        $this->admin = User::factory()->admin()->create();
+        $this->client = User::factory()->client()->create();
+        $this->moderator = User::factory()->moderator()->create();
+
+        $this->userData = User::factory()->make()->toArray();
+        $this->userData['password'] = 'password';
+        $this->userData['password_confirmation'] = 'password';
+    });
+
+    test('admin can edit their own information and other users information', function () {
+        Sanctum::actingAs($this->admin);
+
+        $response = $this->put(route('users.update', $this->admin->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_OK);
+
+
+        $response = $this->put(route('users.update', $this->client->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_OK);
+    });
+
+    test('moderator can edit their own information and other users information but not admin information', function () {
+        Sanctum::actingAs($this->moderator);
+
+
+        $response = $this->put(route('users.update', $this->moderator->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_OK);
+
+
+        $response = $this->put(route('users.update', $this->client->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_OK);
+
+
+        $response = $this->put(route('users.update', $this->admin->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_UNAUTHORIZED);
+    });
+
+    test('client can edit only their own information', function () {
+        Sanctum::actingAs($this->client);
+
+
+        $response = $this->put(route('users.update', $this->client->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_OK);
+
+
+        $response = $this->put(route('users.update', $this->admin->id), $this->userData, ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseAlias::HTTP_UNAUTHORIZED);
+    });
+});
