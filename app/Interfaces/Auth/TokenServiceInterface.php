271f5b756603304e9951a42c8e0ed48eedb78c38
Samuel ANYARONKE <75504658+CodeQuasarr@users.noreply.github.com>
2025-03-16 21:33:08 +0100

    feat(token): modification du TokenService et ajout d'une nouvelle méthode

--- app/Interfaces/Auth/TokenServiceInterface.php
+++ app/Interfaces/Auth/TokenServiceInterface.php
@@ -3,9 +3,16 @@
 namespace App\Interfaces\Auth;
 
 use App\Models\User;
+use Laravel\Sanctum\PersonalAccessToken;
 
 interface TokenServiceInterface
 {
 
-    public function generateToken(User $user): string;
+    public function generateToken(User $user, string $title): string;
+
+    public function generateRefreshToken(User $user, string $title): string;
+
+    public function validateRefreshToken(string $refreshToken): PersonalAccessToken;
+
+    public function deleteExpiredToken(PersonalAccessToken $tokenModel): void;
 }
--- app/Services/Auth/TokenService.php
+++ app/Services/Auth/TokenService.php
@@ -4,11 +4,43 @@ namespace App\Services\Auth;
 
 use App\Interfaces\Auth\TokenServiceInterface;
 use App\Models\User;
+use Carbon\Carbon;
+use Laravel\Sanctum\PersonalAccessToken;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
 
 class TokenService implements TokenServiceInterface
 {
-    public function generateToken(User $user): string
+    public function generateToken(User $user, string $title): string
     {
-        return $user->createToken('api.token')->plainTextToken;
+        return $user->createToken($title, ['*'], now()->addMinutes(15))->plainTextToken;
+    }
+
+    public function generateRefreshToken(User $user, string $title): string
+    {
+        return $user->createToken($title, ['*'], Carbon::now()->addMinutes(15))->plainTextToken;
+    }
+
+    public function validateRefreshToken(string $refreshToken): PersonalAccessToken
+    {
+        if (!$refreshToken) {
+            throw new RuntimeException('Invalid or missing credentials', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $tokenModel = PersonalAccessToken::findToken($refreshToken);
+        if (!$tokenModel || !$tokenModel->can('refresh')) {
+            throw new RuntimeException('Invalid or missing credentials', Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $tokenModel;
+    }
+
+    public function deleteExpiredToken(PersonalAccessToken $tokenModel): void
+    {
+        $expiration = Carbon::parse($tokenModel->expires_at);
+        if ($expiration->isPast()) {
+            $tokenModel->tokenable->tokens()->delete();
+            throw new RuntimeException('Invalid or missing credentials', Response::HTTP_UNAUTHORIZED);
+        }
     }
 }
